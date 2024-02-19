91b3202030a95dd5f16e250ac69a20f03e27c180
PauloGrijp <pxavierf@gmail.com>
2024-02-19 20:25:49 -0300

    feat: implments notifications and validation products

--- src/domain/@shared/notification/notification.spec.ts
+++ src/domain/@shared/notification/notification.spec.ts
@@ -1,6 +1,6 @@
 import Notification from "./notification";
 
-describe("Unit testss for notifications", () => {
+describe("Unit tests for notifications - Customer", () => {
   it("should create errors", () => {
     const notification = new Notification();
     const error = {
@@ -52,6 +52,52 @@ describe("Unit testss for notifications", () => {
     const error = {
       message: "error message",
       context: "customer",
+    };
+    notification.addError(error);
+
+    expect(notification.getErrors()).toEqual([error]);
+  });
+});
+
+describe("Unit tests for notifications - Product", () => {
+  it("should create errors", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
+    };
+
+    notification.addError(error);
+
+    expect(notification.messages("product")).toBe("product: error message,");
+
+    const error2 = {
+      message: "error message2",
+      context: "product",
+    };
+    notification.addError(error2);
+
+    expect(notification.messages("product")).toBe(
+      "product: error message,product: error message2,"
+    );
+  });
+
+  it("should check if notification has at least one error", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
+    };
+    notification.addError(error);
+
+    expect(notification.hasErrors()).toBe(true);
+  });
+
+  it("should get all errors props", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
     };
     notification.addError(error);
 
